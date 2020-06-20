63ca6bf5fe2368798ce6d6fc212282f5f59b1412
Abhinav Jain <abhinav.jain@hotstar.com>
2020-06-20 17:24:53 +0530

    Added media queries for low resolution screen support

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,12 @@
 // Event listeners
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    licenseKey: 'abcd1234-abcd1234-abcd1234-abcd1234',
-    autoScrolling: true,
-    scrollHorizontally: true
+    // licenseKey: 'abcd1234-abcd1234-abcd1234-abcd1234'
+    licenseKey: '8znZmcG#a5',
+    responsiveWidth: 800,
+	responsiveHeight: 768,
+	scrollingSpeed: 450,
+	loopBottom: true,
+	loopHorizontal: true
   });
 });
