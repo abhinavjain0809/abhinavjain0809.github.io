1d57dc0e064a265d3b2ea0f9d784766b606a2c02
Abhinav Jain <abhinav.jain@hotstar.com>
2020-06-21 16:41:06 +0530

    Added dynamic highlighter to the selected page anchor

--- js/custom.js
+++ js/custom.js
@@ -5,8 +5,14 @@ $(document).ready(function() {
     licenseKey: '8znZmcG#a5',
     responsiveWidth: 700,
 	responsiveHeight: 600,
-	scrollingSpeed: 450,
+	// scrollingSpeed: 450,
 	loopBottom: true,
-	loopHorizontal: true
+	loopHorizontal: true,
+
+	// Called every time page changes
+	onLeave: function(origin, destination, direction) {
+		$("#navbar ." + origin.anchor + "-navbar-option").removeClass('selected-navbar-option');
+		$("#navbar ." + destination.anchor + "-navbar-option").addClass('selected-navbar-option');
+	}
   });
 });
