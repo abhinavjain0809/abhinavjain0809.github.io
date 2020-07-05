fc80a5828b95b7ac46b7eaaf3155d6c5c565d5eb
Abhinav Jain <abhinav.jain@hotstar.com>
2020-07-05 15:25:24 +0530

    Completed blog page design + Content for tech and travel populated + Tech is the default category

---
+++ js/constants.js
@@ -0,0 +1,57 @@
+var constants = {
+	indexToCategoryMap: {
+		0: 'tech',
+		1: 'travel',
+		2: 'cupid_tales'
+	},
+
+	allBlogCategoryData: {
+		tech: [{
+			title: "A complete guide to setting up Master and Slave (or multiple) Datasources in Spring",
+			thumbnailUrl: "resources/img/blog/blog_tech_spring_master_slave_datasources.jpg",
+			publishedOn: "Dec 30, 2019",
+			deeplink: "https://medium.com/swlh/a-complete-guide-to-setting-up-multiple-datasources-in-spring-8296d4ff0935"
+		}],
+
+		travel: [{
+			title: "The Queen of all hills - Ooty (Day 3/3)",
+			thumbnailUrl: "resources/img/blog/blog_travel_ooty.jpg",
+			publishedOn: "Jan 27, 2020",
+			deeplink: "https://www.tripoto.com/coonoor/trips/the-queen-of-all-hills-ooty-day-3-3-5e895475241da"
+		},
+		{
+			title: "The Road to Pykara (Day 2/3)",
+			thumbnailUrl: "resources/img/blog/blog_travel_pykara.jpg",
+			publishedOn: "Jan 26, 2020",
+			deeplink: "https://www.tripoto.com/coonoor/trips/the-road-to-pykara-day-2-3-5e6e48bcd2e07"
+		},
+		{
+			title: "The serenity of Coonoor (Day 1/3)",
+			thumbnailUrl: "resources/img/blog/blog_travel_coonoor.jpg",
+			publishedOn: "Jan 25, 2020",
+			deeplink: "https://www.tripoto.com/coonoor/trips/the-serenity-of-coonoor-day-1-3-5e51846ec6ca9"
+		},
+		{
+			title: "Road trip - Rishikesh (1/2)",
+			thumbnailUrl: "resources/img/blog/blog_travel_rishikesh_1.jpg",
+			publishedOn: "Nov 15, 2019",
+			deeplink: "https://www.tripoto.com/uttarakhand/trips/road-trip-rishikesh-1-2-5dd6c74ec01a2"
+		},
+		{
+			title: "Yeh Hai Mumbai Meri Jaan!",
+			thumbnailUrl: "resources/img/blog/blog_travel_mumbai.jpg",
+			publishedOn: "Sep 28, 2019",
+			deeplink: "https://www.tripoto.com/lonavala/trips/yeh-hai-mumbai-meri-jaan-5d94c81e68ab6"
+		},
+		{
+			title: "Varkala - A soulful journey",
+			thumbnailUrl: "resources/img/blog/blog_travel_varkala.jpg",
+			publishedOn: "Jan 25, 2019",
+			deeplink: "https://www.tripoto.com/varkala/trips/varkala-a-soulful-journey-5c5f20e6d0084"
+		}],
+
+		cupid_tales: []
+	},
+
+	blogPostsPerSlide: 4
+}
--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,24 @@
-// Event listeners
+var fullpageAPI = null;
+
 $(document).ready(function() {
-  	
-  	$('#fullpage').fullpage({
+
+	initFullPage();
+
+	// Call this method to populate the content for tech (default) blog page on page load
+  	populateSlides('tech');
+
+  	// Called when cycling through categories on the blogs page
+  	$('.blog-category-select').on('changed.bs.select', function (e, clickedIndex, isSelected, previousValue) {
+  		var categoryName = getCategoryName(clickedIndex);
+
+  		populateSlides(categoryName);
+  		changeBackgroundImage(categoryName);
+	});
+});
+
+function initFullPage() {
+
+	fullpageAPI = new fullpage('#fullpage', {
 	    // licenseKey: 'abcd1234-abcd1234-abcd1234-abcd1234'
 		licenseKey: '8znZmcG#a5',
 	    responsiveWidth: 700,
@@ -22,11 +39,72 @@ $(document).ready(function() {
 				$('#blog-category-wrapper').addClass('hide');
 			}
 		}
-  	});
-
-  	// Called when cycling through categories on the blogs page
-  	$('.blog-category-select').on('changed.bs.select', function (e, clickedIndex, isSelected, previousValue) {
-  		var imgName = 'blog_' + clickedIndex + '_bg.jpg';
-  		document.getElementById("blog").style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0,0,0,0.1)), url('resources/img/" + imgName + "')";
 	});
-});
+
+}
+
+function getCategoryName(clickedIndex) {
+	return constants['indexToCategoryMap'][clickedIndex];
+}
+
+function populateSlides(categoryName) {
+	var blogData = constants['allBlogCategoryData'][categoryName];
+	var totalPostsCount = constants['allBlogCategoryData'][categoryName].length;
+	var currSlide = 1;
+	var slidesRequired = Math.floor(totalPostsCount/constants['blogPostsPerSlide']) + 1;
+	var slidesData = '<div class="slide" data-anchor="blog_' + categoryName + '_' + currSlide + '">';
+	
+	for (i = 1; i <= totalPostsCount; i++) {
+		// slidesData += '<div class="blog-post" onclick="window.open("' + blogData[i-1]['deeplink'] + '","mywindow");">';
+		slidesData += '<div class="blog-post ' + categoryName + '" onclick="window.open(\'' + blogData[i-1]['deeplink'] + '\',\'mywindow\');">';
+		slidesData += '<span class="blog-post-img-container"><img class="blog-post-img" src="' + blogData[i-1]['thumbnailUrl'] + '"></img></span>';
+		slidesData += '<span class="blog-post-desc-container">';
+		slidesData += '<div class="blog-post-title">' + blogData[i-1]['title'] + '</div>';
+		slidesData += '<div class="blog-post-subtext">' + blogData[i-1]['publishedOn'] + '</div>';
+		slidesData += '</span>';
+		slidesData += '</div>';
+
+		// Time to add a new slide
+		if (i % constants['blogPostsPerSlide'] == 0) {
+			slidesData += '</div>';
+			++currSlide;
+
+			// Only add a new slide if there is more content left to populate after the current post
+			if (i != totalPostsCount) {
+				slidesData += '<div class="slide" data-anchor="blog_' + categoryName + '_' + currSlide + '">';
+			}
+		}
+	}
+
+	if (totalPostsCount % constants['blogPostsPerSlide'] != 0) {
+		slidesData += '</div>';
+	}
+
+	document.getElementById("blog").innerHTML = slidesData;
+
+	// remembering the active section / slide
+    var activeSectionIndex = $('.fp-section.active').index();
+    var activeSlideIndex = $('.fp-section.active').find('.slide.active').index();
+
+	fullpageAPI.destroy('all');
+
+    // setting the active section as before
+    $('.section').eq(activeSectionIndex).addClass('active');
+
+    // were we in a slide? Adding the active state again
+    if (activeSlideIndex > -1) {
+        $('.section.active').find('.slide').eq(activeSlideIndex).addClass('active');
+    }
+
+	initFullPage();
+
+	// blog reside at index 3, so we must only show the category dropdown on page 3.
+	if (activeSectionIndex != 3) {
+		$('#blog-category-wrapper').addClass('hide');
+	}
+}
+
+function changeBackgroundImage(categoryName) {
+	var imgName = 'blog_' + categoryName + '_bg.jpg';
+	document.getElementById("blog").style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0,0,0,0.1)), url('resources/img/" + imgName + "')";
+}
